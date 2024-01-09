f7488fad9271583042b54a9f120cac12b1df0f05
nickbytee <nikhilkaushik2228@gmail.com>
2024-01-10 01:11:11 +0530

    Changed whole project database to nosql(mongodb) from sql

---
+++ dom/forgotPassword.js
@@ -0,0 +1,22 @@
+const submitButton = document.getElementById("submit");
+document.addEventListener("submit", resetPassword);
+const backendAPI = "http://localhost:3000";
+
+async function resetPassword(e) {
+  e.preventDefault();
+  const email = document.getElementById("e-mail").value;
+  const myObj = {
+    email: email,
+  };
+  try {
+    const data = await axios.post(
+      `${backendAPI}/password/forgotPassword`,
+      myObj
+    );
+    const message = document.getElementById("message");
+    message.innerHTML = data.data.message;
+    message.style.color = "green";
+  } catch (err) {
+    console.log(err);
+  }
+}
